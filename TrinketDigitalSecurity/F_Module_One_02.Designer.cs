b0cf8d0ace8c4c2a847a524253b490e795e9f5f2
Paiva <danpaiva@live.com>
2021-10-28 23:30:26 -0300

    Ajustes do modulo 01 com botoes | Falta funções

--- TrinketDigitalSecurity/F_Module_One_02.Designer.cs
+++ TrinketDigitalSecurity/F_Module_One_02.Designer.cs
@@ -37,6 +37,7 @@ namespace TrinketDigitalSecurity
             this.lbl_info_c = new System.Windows.Forms.Label();
             this.lbl_info_i = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbl_information
@@ -117,11 +118,21 @@ namespace TrinketDigitalSecurity
             this.button1.Text = "see example";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1140, 649);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "finish";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // F_Module_One_02
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lbl_info_i);
             this.Controls.Add(this.lbl_info_c);
@@ -150,5 +161,6 @@ namespace TrinketDigitalSecurity
         private System.Windows.Forms.Label lbl_info_c;
         private System.Windows.Forms.Label lbl_info_i;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
