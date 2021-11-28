e241ad6c0a82f6d60b21c7d9736104b37c2cdeb9
Daniel Paiva <danpaiva@live.com>
2021-11-28 01:42:07 -0300

    remove files

--- TrinketDigitalSecurity/Properties/Resources.Designer.cs
+++ TrinketDigitalSecurity/Properties/Resources.Designer.cs
@@ -63,29 +63,9 @@ namespace TrinketDigitalSecurity.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Modulo_I___Pop_1 {
-            get {
-                object obj = ResourceManager.GetObject("Modulo I - Pop 1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Prancheta_1_100 {
             get {
                 object obj = ResourceManager.GetObject("Prancheta 1-100", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Prancheta_2_100 {
-            get {
-                object obj = ResourceManager.GetObject("Prancheta 2-100", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
